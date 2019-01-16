3a639213a23dd6824a5dfb88fc525195bf2227d9
Cheng Li <cheng.li@chenglabs.net>
2019-01-16 11:34:01 +0100

    prettier code formatting

--- src/env.config.ts
+++ src/env.config.ts
@@ -1,9 +1,9 @@
-import fs from 'fs';
+import fs from "fs";
 
-let  result:any;
-const configFile=`config/${process.env.NODE_ENV||""}.env`
+let result: any;
+const configFile = `config/${process.env.NODE_ENV || ""}.env`;
 if (fs.existsSync(configFile)) {
-  result = require('dotenv').config({ path: configFile});
+  result = require("dotenv").config({ path: configFile });
   if (result.error) {
     throw result.error;
   }
