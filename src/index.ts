fb25a4d869d39f3aab1c8e12efe59d7c6dda536a
Cheng Li <cheng.li@chenglabs.net>
2019-01-16 09:27:42 +0100

    prettier code formatting

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-import './env.config';
-import '@babel/polyfill/noConflict';
+import "./env.config";
+import "@babel/polyfill/noConflict";
 
+import http from "http";
 
-
-import http from 'http';
-
-const hostname = process.env.HOSTNAME || '127.0.0.1';
+const hostname = process.env.HOSTNAME || "127.0.0.1";
 const port = +(process.env.PORT || "3000");
 
 // tslint:disable-next-line:variable-name
 const server = http.createServer((_req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
 });
 
 server.listen(port, hostname, () => {
