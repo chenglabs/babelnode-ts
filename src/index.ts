f6400193ab0c93248198e383f28abecbd05f5898
Cheng Li <cheng.li@chenglabs.net>
2019-01-22 13:25:46 +0100

    changed index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import http from "http";
 const hostname = process.env.HOSTNAME || "127.0.0.1";
 const port = +(process.env.PORT || "3000");
 
-// tslint:disable-next-line:variable-name
+
 const server = http.createServer((_req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
